544d61b54412f994f3979fe83a6918ba1551372b
nguyencuong2510 <cuong251095@gmail.com>
2018-03-11 18:11:16 +0700

    using AVAudioPlayer

--- AVAudioPlayerDemo/ViewController.swift
+++ AVAudioPlayerDemo/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var showImageVideo: UIView!
+    var  myAudioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let myFilePathString = Bundle.main.path(forResource: "video", ofType: "mp4")
+        if let myFilePathString = myFilePathString
+        {
+            let myFilePathURL = URL(fileURLWithPath: myFilePathString)
+            do{
+                try myAudioPlayer = AVAudioPlayer(contentsOf: myFilePathURL)
+                
+                myAudioPlayer.play()
+            }catch{
+                print("err")
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var myVolumeController: UISlider!
+    
+    @IBAction func stopAudio(sender: AnyObject) {
+        
+        myAudioPlayer.stop()
+        myAudioPlayer.currentTime = 0
+        
     }
-
-
+    
+    @IBAction func pauseAudio(sender: AnyObject) {
+        
+        myAudioPlayer.pause()
+        
+    }
+    
+    @IBAction func playAudio(sender: AnyObject) {
+        
+        myAudioPlayer.play()
+        
+    }
+    
+    @IBAction func volumeControl(sender: AnyObject) {
+        
+        myAudioPlayer.volume=myVolumeController.value
+        
+    }
+    
+    
 }
 
